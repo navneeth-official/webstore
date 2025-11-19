83b49f2b354962e29e5697e7972d5cf0e1b17522
navneeth-official <jnavneethkrishnan@gmail.com>
2025-11-19 20:03:00 +0530

    update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -1,13 +1,5 @@
 module.exports = function (config) {
   config.set({
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
-    plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage'),
-      require('@angular-devkit/build-angular/plugins/karma')
-    ],
     customLaunchers: {
       ChromeHeadlessCI: {
         base: 'ChromeHeadless',
